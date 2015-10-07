6deaaae3f47106e146916a0850bbb63bf21acfad
laarc <sudoarc@gmail.com>
2015-10-06 23:16:08 -0700

    denil; niltree

---
+++ bin/arc.js
@@ -0,0 +1,228 @@
+reader = require("reader");
+compiler = require("compiler");
+ac = function (x, env) {
+  env = env || [];
+  if (ac_string63(x)) {
+    return(ac_string(x, env));
+  } else {
+    if (ac_literal63(x)) {
+      return(x);
+    } else {
+      if (x === "nil") {
+        return(["quote", "nil"]);
+      } else {
+        if (ac_symbol63(x)) {
+          return(ac_var_ref(x, env));
+        } else {
+          if (xcar(x) === "quote") {
+            return(["quote", ac_niltree(cadr(x))]);
+          } else {
+            throw new Error("Bad object in expression " + string(x));
+          }
+        }
+      }
+    }
+  }
+};
+ac_string63 = function (x) {
+  return(string_literal63(x));
+};
+ac_symbol63 = function (x) {
+  return(string63(x));
+};
+ac_string = function (x, env) {
+  return(x);
+};
+ac_literal63 = function (x) {
+  return(boolean63(x) || ac_string63(x) || number63(x) || ! atom63(x) && none63(x));
+};
+xcar = function (x) {
+  if (! atom63(x)) {
+    return(hd(x));
+  }
+};
+xcdr = function (x) {
+  if (! atom63(x)) {
+    return(tl(x));
+  }
+};
+dot = unique(".");
+car = function (x) {
+  if (! x) {
+    return(undefined);
+  } else {
+    if (atom63(x)) {
+      throw new Error("car: expected list, got " + string(x));
+    } else {
+      if (none63(x)) {
+        return(undefined);
+      } else {
+        var v = hd(x);
+        if (v === dot) {
+          throw new Error("car: bad cons " + string(x));
+        } else {
+          return(v);
+        }
+      }
+    }
+  }
+};
+cdr = function (x) {
+  if (! x) {
+    return(undefined);
+  } else {
+    if (atom63(x)) {
+      throw new Error("cdr: expected list, got " + string(x));
+    } else {
+      if (none63(x)) {
+        return(undefined);
+      } else {
+        var v = tl(x);
+        if (xcar(v) === dot) {
+          if (! one63(tl(v))) {
+            throw new Error("cdr: bad cons " + string(x));
+          } else {
+            return(hd(tl(v)));
+          }
+        } else {
+          return(v);
+        }
+      }
+    }
+  }
+};
+cadr = function (x) {
+  return(car(cdr(x)));
+};
+cons = function (x, y) {
+  if (atom63(y)) {
+    if (y) {
+      return([x, dot, y]);
+    } else {
+      return([x]);
+    }
+  } else {
+    return(join([x], y));
+  }
+};
+ac_denil = function (x) {
+  if (! atom63(x)) {
+    return(cons(ac_denil_car(car(x)), ac_denil_cdr(cdr(x))));
+  } else {
+    return(x);
+  }
+};
+ac_denil_car = function (x) {
+  if (x === "nil") {
+    return("nil");
+  } else {
+    return(ac_denil(x));
+  }
+};
+ac_denil_cdr = function (x) {
+  if (x === "nil") {
+    return([]);
+  } else {
+    return(ac_denil(x));
+  }
+};
+ac_niltree = function (x) {
+  if (! atom63(x)) {
+    if (none63(x)) {
+      return("nil");
+    } else {
+      return(cons(ac_niltree(car(x)), ac_niltree(cdr(x))));
+    }
+  } else {
+    if (! x || x === "nil" || x === []) {
+      return("nil");
+    } else {
+      return(x);
+    }
+  }
+};
+var ac_lex63 = function (x, env) {
+  return(in63(x, env));
+};
+var ac_global_name = function (x) {
+  return("_" + x);
+};
+ac_var_ref = function (x, env) {
+  if (ac_lex63(x, env)) {
+    return(x);
+  } else {
+    return(ac_global_name(x));
+  }
+};
+var delimiters = {"(": true, ")": true, "\n": true, ";": true};
+var whitespace = {" ": true, "\n": true, "\t": true};
+var peek_char = function (s) {
+  var _id = s;
+  var pos = _id.pos;
+  var len = _id.len;
+  var string = _id.string;
+  if (pos < len) {
+    return(char(string, pos));
+  }
+};
+var read_char = function (s) {
+  var c = peek_char(s);
+  if (c) {
+    s.pos = s.pos + 1;
+    return(c);
+  }
+};
+var skip_non_code = function (s) {
+  while (true) {
+    var c = peek_char(s);
+    if (nil63(c)) {
+      break;
+    } else {
+      if (whitespace[c]) {
+        read_char(s);
+      } else {
+        if (c === ";") {
+          while (c && !( c === "\n")) {
+            c = read_char(s);
+          }
+          skip_non_code(s);
+        } else {
+          break;
+        }
+      }
+    }
+  }
+};
+var literals = {"-nan": 0 / 0, "false": false, nan: 0 / 0, "true": true, "-inf": -1 / 0, inf: 1 / 0};
+var read_atom = function (s) {
+  var str = "";
+  while (true) {
+    var c = peek_char(s);
+    if (c && (! whitespace[c] && ! delimiters[c])) {
+      str = str + read_char(s);
+    } else {
+      break;
+    }
+  }
+  var x = literals[str];
+  if (is63(x)) {
+    return(x);
+  } else {
+    var n = number(str);
+    if (!( nil63(n) || nan63(n) || inf63(n))) {
+      return(n);
+    } else {
+      return(str);
+    }
+  }
+};
+arc_read = function (s) {
+  var old = reader["read-table"][""];
+  reader["read-table"][""] = read_atom;
+  var r = reader["read-string"](s);
+  reader["read-table"][""] = old;
+  return(r);
+};
+arc_eval = function (expr) {
+  return(eval(ac(expr, [])));
+};
---
+++ bin/arc.lua
@@ -0,0 +1,228 @@
+reader = require("reader")
+compiler = require("compiler")
+function ac(x, env)
+  env = env or {}
+  if ac_string63(x) then
+    return(ac_string(x, env))
+  else
+    if ac_literal63(x) then
+      return(x)
+    else
+      if x == "nil" then
+        return({"quote", "nil"})
+      else
+        if ac_symbol63(x) then
+          return(ac_var_ref(x, env))
+        else
+          if xcar(x) == "quote" then
+            return({"quote", ac_niltree(cadr(x))})
+          else
+            error("Bad object in expression " .. string(x))
+          end
+        end
+      end
+    end
+  end
+end
+function ac_string63(x)
+  return(string_literal63(x))
+end
+function ac_symbol63(x)
+  return(string63(x))
+end
+function ac_string(x, env)
+  return(x)
+end
+function ac_literal63(x)
+  return(boolean63(x) or ac_string63(x) or number63(x) or not atom63(x) and none63(x))
+end
+function xcar(x)
+  if not atom63(x) then
+    return(hd(x))
+  end
+end
+function xcdr(x)
+  if not atom63(x) then
+    return(tl(x))
+  end
+end
+dot = unique(".")
+function car(x)
+  if not x then
+    return(nil)
+  else
+    if atom63(x) then
+      error("car: expected list, got " .. string(x))
+    else
+      if none63(x) then
+        return(nil)
+      else
+        local v = hd(x)
+        if v == dot then
+          error("car: bad cons " .. string(x))
+        else
+          return(v)
+        end
+      end
+    end
+  end
+end
+function cdr(x)
+  if not x then
+    return(nil)
+  else
+    if atom63(x) then
+      error("cdr: expected list, got " .. string(x))
+    else
+      if none63(x) then
+        return(nil)
+      else
+        local v = tl(x)
+        if xcar(v) == dot then
+          if not one63(tl(v)) then
+            error("cdr: bad cons " .. string(x))
+          else
+            return(hd(tl(v)))
+          end
+        else
+          return(v)
+        end
+      end
+    end
+  end
+end
+function cadr(x)
+  return(car(cdr(x)))
+end
+function cons(x, y)
+  if atom63(y) then
+    if y then
+      return({x, dot, y})
+    else
+      return({x})
+    end
+  else
+    return(join({x}, y))
+  end
+end
+function ac_denil(x)
+  if not atom63(x) then
+    return(cons(ac_denil_car(car(x)), ac_denil_cdr(cdr(x))))
+  else
+    return(x)
+  end
+end
+function ac_denil_car(x)
+  if x == "nil" then
+    return("nil")
+  else
+    return(ac_denil(x))
+  end
+end
+function ac_denil_cdr(x)
+  if x == "nil" then
+    return({})
+  else
+    return(ac_denil(x))
+  end
+end
+function ac_niltree(x)
+  if not atom63(x) then
+    if none63(x) then
+      return("nil")
+    else
+      return(cons(ac_niltree(car(x)), ac_niltree(cdr(x))))
+    end
+  else
+    if not x or x == "nil" or x == {} then
+      return("nil")
+    else
+      return(x)
+    end
+  end
+end
+local function ac_lex63(x, env)
+  return(in63(x, env))
+end
+local function ac_global_name(x)
+  return("_" .. x)
+end
+function ac_var_ref(x, env)
+  if ac_lex63(x, env) then
+    return(x)
+  else
+    return(ac_global_name(x))
+  end
+end
+local delimiters = {["("] = true, [")"] = true, ["\n"] = true, [";"] = true}
+local whitespace = {[" "] = true, ["\n"] = true, ["\t"] = true}
+local function peek_char(s)
+  local _id = s
+  local pos = _id.pos
+  local len = _id.len
+  local string = _id.string
+  if pos < len then
+    return(char(string, pos))
+  end
+end
+local function read_char(s)
+  local c = peek_char(s)
+  if c then
+    s.pos = s.pos + 1
+    return(c)
+  end
+end
+local function skip_non_code(s)
+  while true do
+    local c = peek_char(s)
+    if nil63(c) then
+      break
+    else
+      if whitespace[c] then
+        read_char(s)
+      else
+        if c == ";" then
+          while c and not( c == "\n") do
+            c = read_char(s)
+          end
+          skip_non_code(s)
+        else
+          break
+        end
+      end
+    end
+  end
+end
+local literals = {["-nan"] = 0 / 0, ["false"] = false, nan = 0 / 0, ["true"] = true, ["-inf"] = -1 / 0, inf = 1 / 0}
+local function read_atom(s)
+  local str = ""
+  while true do
+    local c = peek_char(s)
+    if c and (not whitespace[c] and not delimiters[c]) then
+      str = str .. read_char(s)
+    else
+      break
+    end
+  end
+  local x = literals[str]
+  if is63(x) then
+    return(x)
+  else
+    local n = number(str)
+    if not( nil63(n) or nan63(n) or inf63(n)) then
+      return(n)
+    else
+      return(str)
+    end
+  end
+end
+function arc_read(s)
+  local old = reader["read-table"][""]
+  reader["read-table"][""] = read_atom
+  local r = reader["read-string"](s)
+  reader["read-table"][""] = old
+  return(r)
+end
+function arc_eval(expr)
+  return(eval(ac(expr, {})))
+end
